c2fe3f1401edad8be48ffdc1ee5dd7e5fb794301
Vikrant <124357732+VikrantChugh@users.noreply.github.com>
2024-04-03 00:22:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ import threading
 from ServiceAccount import get_service_account_details
 from AvailabilityZones import get_availability_zone_details
 from virtual_machine import get_all_vm_details
+from StorageVolume import get_volumes
 def main():
     VM=threading.Thread(target=get_all_vm_details)
     SA =threading.Thread(target=get_service_account_details)
     Zone =threading.Thread(target=get_availability_zone_details)
+    Vol=threading.Thread(target=get_volumes)
     Zone.start()
     Zone.join()
     SA.start()
     SA.join()
     VM.start()
     VM.join()
+    Vol.start()
+    Vol.join()
+
 
 main()
