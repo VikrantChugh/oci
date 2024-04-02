4973c1ff9428f0a1b15db9b5a70315757e073ac0
Vikrant <124357732+VikrantChugh@users.noreply.github.com>
2024-04-02 14:31:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import threading
+# from oci-availability_zone import get_availability_zone_details
+# from oci-service_account import get_service_account_details
+
+from ServiceAccount import get_service_account_details
+from AvailabilityZones import get_availability_zone_details
+from virtual_machine import get_all_vm_details
+def main():
+    VM=threading.Thread(target=get_all_vm_details)
+    SA =threading.Thread(target=get_service_account_details)
+    Zone =threading.Thread(target=get_availability_zone_details)
+    Zone.start()
+    Zone.join()
+    SA.start()
+    SA.join()
+    VM.start()
+    VM.join()
+
+main()
