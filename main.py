e33e5aeece23ad76d15f89e6d64a1586bb51a38c
Vikrant <124357732+VikrantChugh@users.noreply.github.com>
2024-04-03 10:28:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ from ServiceAccount import get_service_account_details
 from AvailabilityZones import get_availability_zone_details
 from virtual_machine import get_all_vm_details
 from StorageVolume import get_volumes
+from Network import get_network_details
 def main():
     VM=threading.Thread(target=get_all_vm_details)
     SA =threading.Thread(target=get_service_account_details)
     Zone =threading.Thread(target=get_availability_zone_details)
     Vol=threading.Thread(target=get_volumes)
+    Network=threading.Thread(target=get_network_details)
     Zone.start()
     Zone.join()
     SA.start()
@@ -19,6 +21,9 @@ def main():
     VM.join()
     Vol.start()
     Vol.join()
+    Network.start()
+    Network.join()
+    
 
 
 main()
